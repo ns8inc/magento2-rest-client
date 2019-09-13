4763591e930d6c618ef2220b487b928a478e791c
Christopher Froehlich <christopher.r.froehlich@gmail.com>
2019-09-13 12:33:17 -0400

    Simplify module exports.

--- index.js
+++ index.js
@@ -1,10 +1,2 @@
 'use strict';
-
-const RestClient = require('./dist/core/RestClient.js').RestClient;
-
 module.exports = require('./dist/index')
-
-module.exports.Magento2Client = function (options) {
-  var client = new RestClient(options);
-  return client;
-};
