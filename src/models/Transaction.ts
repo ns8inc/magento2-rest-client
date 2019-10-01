921b627cd268d3f9363f4986dead5708d5122563
Christopher Froehlich <christopher.r.froehlich@gmail.com>
2019-10-01 14:54:55 -0400

    Fixed copy/paste issue with property names on Transaction interface. Property names are lower_case_underscore always.

--- src/models/Transaction.ts
+++ src/models/Transaction.ts
@@ -7,18 +7,18 @@ export interface TransactionData {
 }
 
 export interface Transaction {
-  additionalInformation: string[] | null;
-  childTransactions: Transaction[];
-  createdAt: string;
-  extensionAttributes: TransactionExtensionAttributes | null;
-  isClosed: number;
-  orderID: number;
-  parentID: number | null;
-  parentTxnID: string;
-  paymentID: number;
-  transactionID: number;
-  txnID: string;
-  txnType: TxnType;
+  additional_information: string[] | null;
+  child_transactions: Transaction[];
+  created_at: string;
+  extension_attributes: TransactionExtensionAttributes | null;
+  is_closed: number;
+  order_id: number;
+  parent_id: number | null;
+  parent_txn_id: string;
+  payment_id: number;
+  transaction_id: number;
+  txn_id: string;
+  txn_type: TxnType;
 }
 
 export enum TxnType {
