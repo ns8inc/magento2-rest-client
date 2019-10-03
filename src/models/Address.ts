8843bfa52811f556d3f3ef18d4a61437a99de692
Christopher Froehlich <christopher.froehlich@ns8.com>
2019-10-02 21:02:33 -0400

    Adding default shipping and billing to address model.

--- src/models/Address.ts
+++ src/models/Address.ts
@@ -10,28 +10,30 @@ export interface AttributeValueField {
 }
 
 export interface Address {
-  address_type: string;
-  city: string;
+  address_type?: string;
+  city?: string;
   company?: string;
-  country_id: string;
+  country_id?: string;
   customer_address_id?: number;
   customer_id?: number;
-  email: string;
+  default_billing?: boolean;
+  default_shipping?: boolean;
+  email?: string;
   entity_id: number;
   extension_attributes?: BillingAddressExtensionAttributes;
   fax?: string;
-  firstname: string;
-  lastname: string;
+  firstname?: string;
+  lastname?: string;
   middlename?: string;
   parent_id: number;
-  postcode: string;
+  postcode?: string;
   prefix?: string;
-  region_code: string;
-  region_id: number;
-  region: Region;
-  street: string[];
+  region_code?: string;
+  region_id?: number;
+  region?: Region;
+  street?: string[];
   suffix?: string;
-  telephone: string;
+  telephone?: string;
   vat_id?: string;
   vat_is_valid?: number;
   vat_request_date?: string;
