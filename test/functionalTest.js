209796d1df3d5485355c4c276a3a9255759a8c90
Christopher Froehlich <christopher.r.froehlich@gmail.com>
2019-10-08 08:38:47 -0400

    Locking down versions. Added typedoc support.

--- test/functionalTest.js
+++ test/functionalTest.js
@@ -1,20 +1,26 @@
 const app = require('../dist/app');
 const RestClient = app.RestClient;
 const fs = require('fs');
+require('dotenv').config();
 
 const options = {
-  url: 'http://dev-cfroehlich.ns8demos.com/index.php/rest',
-  consumerKey: 'm9aj4z9q6b6z0ss708pvoxch22wkbqse',
-  consumerSecret: 'c1k1l4ayzum511141ylk6w9qfkhtka7h',
-  accessToken: '0xfw294s73dh8yo5ewoe3p18jofhicpf',
-  accessTokenSecret: 'auroj9em9i196hy7d1uzyqini8gdtmet'
+    url: process.env.MAGENTO_URL || '',
+    consumerKey: process.env.CONSUMER_KEY || '',
+    consumerSecret: process.env.CONSUMER_SECRET || '',
+    accessToken: process.env.ACCESS_TOKEN || '',
+    accessTokenSecret: process.env.ACCESS_TOKEN_SECRET || '',
 };
 const client = new RestClient(options);
+
+if(!fs.existsSync('test/model_data')) fs.mkdirSync('test/model_data');
+if(!fs.existsSync('test/model_data/orders')) fs.mkdirSync('test/model_data/orders');
+if(!fs.existsSync('test/model_data/transactions')) fs.mkdirSync('test/model_data/transactions');
 
 client.orders.list()
   .then(function (data) {
     try {
       fs.writeFileSync('test/model_data/orders/orders.json', JSON.stringify(data, null, 2))
+      console.log('Found all orders')
       data.items.forEach((item) => {
         try {
           client.orders.get(item.entity_id)
@@ -33,6 +39,7 @@ client.transactions.list()
   .then(function (data) {
     try {
       fs.writeFileSync('test/model_data/transactions/transactions.json', JSON.stringify(data, null, 2))
+      console.log('Found all transactions')
       data.items.forEach((item) => {
         try {
           client.transactions.getByTransactionId(item.txn_id)
@@ -50,8 +57,10 @@ client.transactions.list()
 client.customers.get(1)
   .then(function (data) {
     fs.writeFileSync('test/model_data/customer_1.json', JSON.stringify(data, null, 2))
+    console.log('Found customer')
   })
 client.addresses.get(1)
   .then(function (data) {
     fs.writeFileSync('test/model_data/address_1.json', JSON.stringify(data, null, 2))
+    console.log('Found address')
   })
