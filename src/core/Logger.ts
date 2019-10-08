7d86b5f26230c6fbc1818536c1f57beef41179d0
Christopher Froehlich <christopher.r.froehlich@gmail.com>
2019-10-08 16:43:44 -0400

    Attempt to better handle and control and log errors; try to return an actual HTTP error that can be processed by consumers.

--- src/core/Logger.ts
+++ src/core/Logger.ts
@@ -1,25 +1,51 @@
-const winston = require('winston');
+import { createLogger, format, transports } from 'winston';
+import * as StackTracey from 'stacktracey';
+const { combine, timestamp, label, prettyPrint, json, colorize, errors } = format;
 
 export class Logger {
   public logger: any;
   constructor() {
-    this.logger = new winston.createLogger({
-      transports: [
-        new winston.transports.Console({
-          level: 'debug',
-          handleExceptions: true,
-          json: false,
+    this.logger = createLogger({
+      format: combine(
+        label({ label: 'Magento' }),
+        timestamp(),
+        prettyPrint({
           colorize: true
+        }),
+        json(),
+        colorize({
+          colors: {info: 'blue', error: 'red'}
+        })
+      ),
+      transports: [
+        new transports.Console({
+          level: 'debug',
+          handleExceptions: true
+        }),
+        new transports.File({
+          filename: 'request.log',
+        }),
+        new transports.File({
+          filename: 'error.log',
+          level: 'error'
         })
       ],
       exitOnError: false
     });
-    this.logger.info('Winston logging library initialized.');
+    this.logger.info('Magento 2 REST Client logging initialized.');
   }
-  info(message) {
+  public info(message): void {
     this.logger.info(message);
   }
-  error(message) {
+  public error(message, err): void {
     this.logger.error(message);
+    if (err) {
+      const errorsFormat = errors({stack: false});
+      if (!err.stack) {
+        err.stack = new StackTracey().pretty;
+      }
+      const info = errorsFormat.transform(err, err.stack);
+      this.logger.error(info);
+    }
   }
 }
